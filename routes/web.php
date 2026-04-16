14f327ce7a5e674ad4fed8d5a8ec218bea9ec527
moisesvela2705 <moisesvela2705@gmail.com>
2026-04-16 12:51:24 -0600

    web

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome.blade.php'); // o tu vista real
+    return view('catalog.blade.php'); // o tu vista real
 });
